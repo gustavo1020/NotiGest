5b0dcd5287e5a7a085e7fbc4fde21dd67a275cde
gustavo1020 <49031933+gustavo1020@users.noreply.github.com>
2024-01-23 18:54:13 -0300

    Update Program.cs

--- NotiGest/Program.cs
+++ NotiGest/Program.cs
@@ -10,6 +10,14 @@ using NotiGest.Configurations;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
+builder.WebHost.UseKestrel();
+builder.WebHost.UseIIS();
+
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
